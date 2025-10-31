25bec90f66dd56ae2402871d406963b937c9dc35
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2025-11-01 00:23:07 +0100

    Improve order-checking tests to make sure that [ord-1]*P != O

--- tests/params.rs
+++ tests/params.rs
@@ -3,7 +3,7 @@
 use fp2::traits::Fp2 as FpTrait;
 use num_bigint::BigUint;
 use poke::{
-    PublicParams, SUCCESS_RETVAL,
+    FAILURE_RETVAL, PublicParams, SUCCESS_RETVAL,
     params::{poke_i, poke_iii, poke_v},
 };
 use rstest::rstest;
@@ -28,30 +28,25 @@ fn starting_curve_has_j_invariant_1728<Fp2: FpTrait>(#[case] params: PublicParam
 #[case::poke_level_iii(poke_iii::get_params())]
 #[case::poke_level_v(poke_v::get_params())]
 fn torsion_basis_points_have_correct_order<Fp2: FpTrait>(#[case] params: PublicParams<Fp2>) {
-    // Represent 3^b and 5^c as byte arrays
-    let three_torsion_order = BigUint::from(3u8).pow(
-        params
-            .three_torsion_exp
-            .try_into()
-            .expect("Exponent of the 3-torsion subgroup is too big to fit in a u32 (we do not ever expect this to be the case)")
-        );
-    let three_torsion_order_bitsize = three_torsion_order.bits().try_into().expect(
-        "Size in bits of 3^b is too big to fit in a usize (we do not ever expect this to happen)",
-    );
-    let three_torsion_order = three_torsion_order.to_bytes_le();
+    let ONE = BigUint::from(1u8);
 
-    let five_torsion_order = BigUint::from(5u8).pow(
-        params
-            .five_torsion_exp
-            .try_into()
-            .expect("Exponent of the 5-torsion subgroup is too big to fit in a u32 (we do not ever expect this to be the case)")
-        );
-    let five_torsion_order_bitsize = five_torsion_order.bits().try_into().expect(
-        "Size in bits of 5^c is too big to fit in a usize (we do not ever expect this to happen)",
+    // Ensure [2^a] * (P, Q, P - Q) = O, and [2^a - 1] * (P, Q, P - Q) != O
+    let two_torsion_basis_times_one_less_than_order = params
+        .starting_curve
+        .basis_double_iter(&params.two_torsion_basis, params.two_torsion_exp - 1);
+    assert_eq!(
+        two_torsion_basis_times_one_less_than_order.P.is_zero(),
+        FAILURE_RETVAL
     );
-    let five_torsion_order = five_torsion_order.to_bytes_le();
+    assert_eq!(
+        two_torsion_basis_times_one_less_than_order.Q.is_zero(),
+        FAILURE_RETVAL
+    );
+    assert_eq!(
+        two_torsion_basis_times_one_less_than_order.PQ.is_zero(),
+        FAILURE_RETVAL
+    );
 
-    // Compute [2^a] * (P, Q, P - Q)
     let two_torsion_basis_times_order = params
         .starting_curve
         .basis_double_iter(&params.two_torsion_basis, params.two_torsion_exp);
@@ -59,41 +54,79 @@ fn torsion_basis_points_have_correct_order<Fp2: FpTrait>(#[case] params: PublicP
     assert_eq!(two_torsion_basis_times_order.Q.is_zero(), SUCCESS_RETVAL);
     assert_eq!(two_torsion_basis_times_order.PQ.is_zero(), SUCCESS_RETVAL);
 
-    // Compute [3^b] * (R, S, R - S)
+    // Ensure [3^b] * (R, S, R - S) - O, and [3^b - 1] * (R, S, R - S) != O
+    let xR_times_one_less_than_order = params.starting_curve.xmul(
+        &params.three_torsion_basis.P,
+        &(&params.three_torsion_order - &ONE).to_bytes_le(),
+        params.three_torsion_order.bits() as usize,
+    );
+    let xS_times_one_less_than_order = params.starting_curve.xmul(
+        &params.three_torsion_basis.Q,
+        &(&params.three_torsion_order - &ONE).to_bytes_le(),
+        params.three_torsion_order.bits() as usize,
+    );
+    let xRS_times_one_less_than_order = params.starting_curve.xmul(
+        &params.three_torsion_basis.PQ,
+        &(&params.three_torsion_order - &ONE).to_bytes_le(),
+        params.three_torsion_order.bits() as usize,
+    );
+    assert_eq!(xR_times_one_less_than_order.is_zero(), FAILURE_RETVAL);
+    assert_eq!(xS_times_one_less_than_order.is_zero(), FAILURE_RETVAL);
+    assert_eq!(xRS_times_one_less_than_order.is_zero(), FAILURE_RETVAL);
+
     let xR_times_order = params.starting_curve.xmul(
         &params.three_torsion_basis.P,
-        &three_torsion_order,
-        three_torsion_order_bitsize,
+        &params.three_torsion_order.to_bytes_le(),
+        params.three_torsion_order.bits() as usize,
     );
     let xS_times_order = params.starting_curve.xmul(
         &params.three_torsion_basis.Q,
-        &three_torsion_order,
-        three_torsion_order_bitsize,
+        &params.three_torsion_order.to_bytes_le(),
+        params.three_torsion_order.bits() as usize,
     );
     let xRS_times_order = params.starting_curve.xmul(
         &params.three_torsion_basis.PQ,
-        &three_torsion_order,
-        three_torsion_order_bitsize,
+        &params.three_torsion_order.to_bytes_le(),
+        params.three_torsion_order.bits() as usize,
     );
     assert_eq!(xR_times_order.is_zero(), SUCCESS_RETVAL);
     assert_eq!(xS_times_order.is_zero(), SUCCESS_RETVAL);
     assert_eq!(xRS_times_order.is_zero(), SUCCESS_RETVAL);
 
-    // Compute [5^c] * (X, Y, X - Y)
+    // Ensure [5^c] * (X, Y, X - Y) = O and [5^c - 1] * (X, Y, X - Y) != O
+    let xX_times_one_less_than_order = params.starting_curve.xmul(
+        &params.five_torsion_basis.P,
+        &(&params.five_torsion_order - &ONE).to_bytes_le(),
+        params.five_torsion_order.bits() as usize,
+    );
+    let xY_times_one_less_than_order = params.starting_curve.xmul(
+        &params.five_torsion_basis.Q,
+        &(&params.five_torsion_order - &ONE).to_bytes_le(),
+        params.five_torsion_order.bits() as usize,
+    );
+    let xXY_times_one_less_than_order = params.starting_curve.xmul(
+        &params.five_torsion_basis.PQ,
+        &(&params.five_torsion_order - &ONE).to_bytes_le(),
+        params.five_torsion_order.bits() as usize,
+    );
+    assert_eq!(xX_times_one_less_than_order.is_zero(), FAILURE_RETVAL);
+    assert_eq!(xY_times_one_less_than_order.is_zero(), FAILURE_RETVAL);
+    assert_eq!(xXY_times_one_less_than_order.is_zero(), FAILURE_RETVAL);
+
     let xX_times_order = params.starting_curve.xmul(
         &params.five_torsion_basis.P,
-        &five_torsion_order,
-        five_torsion_order_bitsize,
+        &params.five_torsion_order.to_bytes_le(),
+        params.five_torsion_order.bits() as usize,
     );
     let xY_times_order = params.starting_curve.xmul(
         &params.five_torsion_basis.Q,
-        &five_torsion_order,
-        five_torsion_order_bitsize,
+        &params.five_torsion_order.to_bytes_le(),
+        params.five_torsion_order.bits() as usize,
     );
     let xXY_times_order = params.starting_curve.xmul(
         &params.five_torsion_basis.PQ,
-        &five_torsion_order,
-        five_torsion_order_bitsize,
+        &params.five_torsion_order.to_bytes_le(),
+        params.five_torsion_order.bits() as usize,
     );
     assert_eq!(xX_times_order.is_zero(), SUCCESS_RETVAL);
     assert_eq!(xY_times_order.is_zero(), SUCCESS_RETVAL);

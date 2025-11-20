d755b7b4e6b00fe21b5763b76ea5357428f77b76
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2025-11-20 14:56:14 +0100

    Make small output formatting modifications

--- examples/poke_i.rs
+++ examples/poke_i.rs
@@ -9,11 +9,11 @@ fn main() {
 
     let mut message = String::from("Hello, world!");
     let message = unsafe { message.as_bytes_mut() };
-    println!("Original message: {:?}\n", message);
+    println!("Original message: {:?}", message);
 
     let (mut ct, ok) = encrypt(&params, &pub_key, message);
     assert_eq!(ok, SUCCESS_RETVAL, "Encryption finished with errors");
-    println!("\nEncrypted message: {:?}\n", ct.encrypted_message);
+    println!("\nEncrypted message: {:?}", ct.encrypted_message);
 
     let (message, ok) = decrypt(&params, &prv_key, &mut ct);
     assert_eq!(ok, SUCCESS_RETVAL, "Decryption finished with errors");

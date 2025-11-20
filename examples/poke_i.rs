d1130a6118f58527a666b9bff7d998c655f1190b
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2025-11-20 13:45:55 +0100

    Added decryption step to POKE level I example

--- examples/poke_i.rs
+++ examples/poke_i.rs
@@ -1,15 +1,21 @@
 #![allow(non_snake_case)]
 
-use poke::{SUCCESS_RETVAL, encrypt, example_keypairs, params};
+use poke::{SUCCESS_RETVAL, decrypt, encrypt, example_keypairs, params};
 
 fn main() {
     let params = params::poke_i::get_params();
     let pub_key = example_keypairs::poke_i::get_pub_key();
+    let prv_key = example_keypairs::poke_i::get_prv_key();
 
     let mut message = String::from("Hello, world!");
     let message = unsafe { message.as_bytes_mut() };
-    let (ct, ok) = encrypt(&params, &pub_key, message);
+    println!("Original message: {:?}\n", message);
 
+    let (mut ct, ok) = encrypt(&params, &pub_key, message);
     assert_eq!(ok, SUCCESS_RETVAL, "Encryption finished with errors");
-    println!("Encrypted message: {:?}", ct.encrypted_message);
+    println!("\nEncrypted message: {:?}\n", ct.encrypted_message);
+
+    let (message, ok) = decrypt(&params, &prv_key, &mut ct);
+    assert_eq!(ok, SUCCESS_RETVAL, "Decryption finished with errors");
+    println!("\nDecrypted message: {:?}", message);
 }

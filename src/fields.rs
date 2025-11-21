3be20fdcd4bacab9c2615c9533212053ba14d840
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2025-11-21 12:11:04 +0100

    Add Fp2 field definitions for INKE
    This is in preparation for implementing the INKE encryption and
    decryption

--- src/fields.rs
+++ src/fields.rs
@@ -60,6 +60,66 @@ fp2::define_fp2_from_modulus!(
     modulus = POKE_V_MODULUS,
 );
 
+// INKE level I: p = 2^128 * 3^162 * 127 - 1
+const INKE_I_MODULUS: [u64; 7] = [
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xbd94329674e2c7f6,
+    0x0422d789525e827b,
+    0xa7c33fd124a5e074,
+    0xa8c4c5999e1b37e1,
+    0x00000000000000d7,
+];
+
+fp2::define_fp2_from_modulus!(
+    typename = InkeFieldI,
+    base_typename = InkeFieldIBase,
+    modulus = INKE_I_MODULUS,
+);
+
+// INKE level III: p = 2^192 * 3^243 * 5 * 7 - 1
+const INKE_III_MODULUS: [u64; 10] = [
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0x6e6122aa036cb470,
+    0x4a1cb212d489e150,
+    0x1c5f1167f5c36a6c,
+    0xa9f4e8afc92b3fe6,
+    0x7975d4a56c357241,
+    0x72e38a9838bafd79,
+    0x000000000000004d,
+];
+
+fp2::define_fp2_from_modulus!(
+    typename = InkeFieldIII,
+    base_typename = InkeFieldIIIBase,
+    modulus = INKE_III_MODULUS,
+);
+
+// INKE level V: p = 2^257 * 3^324 * 7^2 - 1
+const INKE_V_MODULUS: [u64; 13] = [
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xa83d19cb1c168101,
+    0x5fb91904b4b3c20e,
+    0x1a33717474b5ba03,
+    0xb40334f01f15c342,
+    0x73d4791f96fe9df3,
+    0x84e53b06dea8956d,
+    0x873beecd981293dd,
+    0x96b6775d9e922f87,
+    0x000000000000011a,
+];
+
+fp2::define_fp2_from_modulus!(
+    typename = InkeFieldV,
+    base_typename = InkeFieldVBase,
+    modulus = INKE_V_MODULUS,
+);
+
 #[cfg(test)]
 mod tests {
     mod poke_i {
@@ -81,5 +141,26 @@ mod tests {
 
         fp2::define_fp_tests!(PokeFieldVBase);
         fp2::define_fp2_tests!(PokeFieldV, POKE_V_MODULUS, 5);
+    }
+
+    mod inke_i {
+        use super::super::{INKE_I_MODULUS, InkeFieldI, InkeFieldIBase};
+
+        fp2::define_fp_tests!(InkeFieldIBase);
+        fp2::define_fp2_tests!(InkeFieldI, INKE_I_MODULUS, 5);
+    }
+
+    mod inke_iii {
+        use super::super::{INKE_III_MODULUS, InkeFieldIII, InkeFieldIIIBase};
+
+        fp2::define_fp_tests!(InkeFieldIIIBase);
+        fp2::define_fp2_tests!(InkeFieldIII, INKE_III_MODULUS, 5);
+    }
+
+    mod inke_v {
+        use super::super::{INKE_V_MODULUS, InkeFieldV, InkeFieldVBase};
+
+        fp2::define_fp_tests!(InkeFieldVBase);
+        fp2::define_fp2_tests!(InkeFieldV, INKE_V_MODULUS, 5);
     }
 }

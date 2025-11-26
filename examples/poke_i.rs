fe6c24ab003b69cfe5b206d930d16b9cb2019e1b
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2025-11-26 15:16:31 +0100

    Remove older relics of previous implementation from POKE level I example
    Where we modified the message buffer in-place when encrypting/decrypting

--- examples/poke_i.rs
+++ examples/poke_i.rs
@@ -7,15 +7,15 @@ fn main() {
     let pub_key = example_keypairs::poke_i::get_pub_key();
     let prv_key = example_keypairs::poke_i::get_prv_key();
 
-    let mut message = String::from("Hello, world!");
-    let message = unsafe { message.as_bytes_mut() };
+    let message = String::from("Hello, world!");
+    let message = message.as_bytes();
     println!("Original message: {:?}", message);
 
-    let (mut ct, ok) = encrypt(&params, &pub_key, message);
+    let (ct, ok) = encrypt(&params, &pub_key, message);
     assert_eq!(ok, SUCCESS_RETVAL, "Encryption finished with errors");
     println!("\nEncrypted message: {:?}", ct.encrypted_message);
 
-    let (message, ok) = decrypt(&params, &prv_key, &mut ct);
+    let (message, ok) = decrypt(&params, &prv_key, &ct);
     assert_eq!(ok, SUCCESS_RETVAL, "Decryption finished with errors");
     println!("\nDecrypted message: {:?}", message);
 }

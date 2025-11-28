681b4df199c40cc289e7e89514c5d8a21befdb65
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2025-11-28 15:38:03 +0100

    Add power-of-5 specific dlog functions and benchmark them to see how they compare

---
+++ benches/dlp.rs
@@ -0,0 +1,117 @@
+#![allow(non_snake_case)]
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use fp2::traits::{Fp as _, Fp2 as Fp2Trait};
+use num_bigint::BigUint;
+use poke::{
+    PublicParams,
+    dlp::{
+        solve_dlp_order_five, solve_dlp_order_power_of_five, solve_dlp_small_prime_order,
+        solve_dlp_small_prime_power_order,
+    },
+    params::{poke_i, poke_iii, poke_v},
+    rand::sample_random_torsion_basis,
+};
+
+// Sample a generator of a subgroup of Fp^2 of order 5^exp
+fn generate_fp2_power_of_five_subgroup_generator<Fp2: Fp2Trait>(
+    pub_params: PublicParams<Fp2>,
+    exp: usize,
+) -> Fp2 {
+    let FIVE = BigUint::from(5u8);
+
+    let torsion_order = &FIVE.pow(exp.try_into().unwrap());
+    let cofactor_order = &pub_params.five_torsion_cofactor
+        * &FIVE.pow((pub_params.five_torsion_exp - exp).try_into().unwrap());
+
+    // The Weil pairing of a basis of the n-torsion subgroup will be the generator of an order-n subgroup of Fp^2
+    let (_, _, eUV) =
+        sample_random_torsion_basis(&pub_params.starting_curve, &torsion_order, &cofactor_order);
+
+    eUV
+}
+
+fn dlp_prime_order(c: &mut Criterion) {
+    const PRIME: usize = 5;
+    const EXP: usize = 1;
+    const LOG: [u8; 1] = [3];
+    const LOG_BITSIZE: usize = 2;
+
+    let gen_i = generate_fp2_power_of_five_subgroup_generator(poke_i::get_params(), EXP);
+    let val_i = gen_i.pow(&LOG, LOG_BITSIZE);
+    let gen_iii = generate_fp2_power_of_five_subgroup_generator(poke_iii::get_params(), EXP);
+    let val_iii = gen_iii.pow(&LOG, LOG_BITSIZE);
+    let gen_v = generate_fp2_power_of_five_subgroup_generator(poke_v::get_params(), EXP);
+    let val_v = gen_v.pow(&LOG, LOG_BITSIZE);
+
+    let mut group_generic =
+        c.benchmark_group("Discrete log in groups of prime order (= 5), using generic method");
+    group_generic.bench_function("POKÉ level I", |b| {
+        b.iter(|| solve_dlp_small_prime_order(&gen_i, &val_i, PRIME))
+    });
+    group_generic.bench_function("POKÉ level III", |b| {
+        b.iter(|| solve_dlp_small_prime_order(&gen_iii, &val_iii, PRIME))
+    });
+    group_generic.bench_function("POKÉ level V", |b| {
+        b.iter(|| solve_dlp_small_prime_order(&gen_v, &val_v, PRIME))
+    });
+    group_generic.finish();
+
+    let mut group_specialized =
+        c.benchmark_group("Discrete log in groups of prime order (= 5), using specialized method");
+    group_specialized.bench_function("POKÉ level I", |b| {
+        b.iter(|| solve_dlp_order_five(&gen_i, &val_i))
+    });
+    group_specialized.bench_function("POKÉ level III", |b| {
+        b.iter(|| solve_dlp_order_five(&gen_iii, &val_iii))
+    });
+    group_specialized.bench_function("POKÉ level V", |b| {
+        b.iter(|| solve_dlp_order_five(&gen_v, &val_v))
+    });
+    group_specialized.finish();
+}
+
+fn dlp_prime_power_order(c: &mut Criterion) {
+    const PRIME: usize = 5;
+    const EXP: usize = 13;
+    const LOG: [u8; 4] = [21, 205, 91, 7]; // 123456789
+    const LOG_BITSIZE: usize = 27;
+
+    let gen_i = generate_fp2_power_of_five_subgroup_generator(poke_i::get_params(), EXP);
+    let val_i = gen_i.pow(&LOG, LOG_BITSIZE);
+    let gen_iii = generate_fp2_power_of_five_subgroup_generator(poke_iii::get_params(), EXP);
+    let val_iii = gen_iii.pow(&LOG, LOG_BITSIZE);
+    let gen_v = generate_fp2_power_of_five_subgroup_generator(poke_v::get_params(), EXP);
+    let val_v = gen_v.pow(&LOG, LOG_BITSIZE);
+
+    let mut group_generic = c.benchmark_group(
+        "Discrete log in groups of prime-power order (= 5^c), using generic method",
+    );
+    group_generic.bench_function("POKÉ level I", |b| {
+        b.iter(|| solve_dlp_small_prime_power_order(&gen_i, &val_i, PRIME, EXP))
+    });
+    group_generic.bench_function("POKÉ level III", |b| {
+        b.iter(|| solve_dlp_small_prime_power_order(&gen_iii, &val_iii, PRIME, EXP))
+    });
+    group_generic.bench_function("POKÉ level V", |b| {
+        b.iter(|| solve_dlp_small_prime_power_order(&gen_v, &val_v, PRIME, EXP))
+    });
+    group_generic.finish();
+
+    let mut group_specialized = c.benchmark_group(
+        "Discrete log in groups of prime-power order (= 5^c), using specialized method",
+    );
+    group_specialized.bench_function("POKÉ level I", |b| {
+        b.iter(|| solve_dlp_order_power_of_five(&gen_i, &val_i, EXP))
+    });
+    group_specialized.bench_function("POKÉ level III", |b| {
+        b.iter(|| solve_dlp_order_power_of_five(&gen_iii, &val_iii, EXP))
+    });
+    group_specialized.bench_function("POKÉ level V", |b| {
+        b.iter(|| solve_dlp_order_power_of_five(&gen_v, &val_v, EXP))
+    });
+    group_specialized.finish();
+}
+
+criterion_group!(benches, dlp_prime_order, dlp_prime_power_order);
+criterion_main!(benches);
--- src/dlp.rs
+++ src/dlp.rs
@@ -76,6 +76,91 @@ pub fn solve_dlp_small_prime_power_order<Fp2: Fp2Trait>(
     (word_bn_to_byte_bn(&partial_sum), retval)
 }
 
+pub fn solve_dlp_order_five<Fp2: Fp2Trait>(generator: &Fp2, value: &Fp2) -> (u8, u32) {
+    let mut retval = FAILURE_RETVAL;
+
+    let mut element = Fp2::ONE;
+    let mut result = 0;
+
+    let found_log = value.equals(&element);
+    result |= 0 & (found_log as u8);
+    retval |= found_log;
+
+    element *= *generator;
+
+    let found_log = value.equals(&element);
+    result |= 1 & (found_log as u8);
+    retval |= found_log;
+
+    let mut element2 = element * *generator;
+
+    let found_log = value.equals(&element2);
+    result |= 2 & (found_log as u8);
+    retval |= found_log;
+
+    element2.set_invert();
+
+    let found_log = value.equals(&element2);
+    result |= 3 & (found_log as u8);
+    retval |= found_log;
+
+    element.set_invert();
+
+    let found_log = value.equals(&element);
+    result |= 4 & (found_log as u8);
+    retval |= found_log;
+
+    (result, retval)
+}
+
+// WARN: Vartime with respect to public parameters (the order of the 5^c-torsion subgroup)
+pub fn solve_dlp_order_power_of_five<Fp2: Fp2Trait>(
+    generator: &Fp2,
+    value: &Fp2,
+    e: usize,
+) -> (BigNum, u32) {
+    let mut retval = SUCCESS_RETVAL;
+
+    let p_to_the_e_basis = (0..=e)
+        .map(|exp| word_bn_to_byte_bn(&prime_power_to_bn_vartime(p, exp)))
+        .collect::<Vec<_>>();
+
+    let prime_order_subgroup_generator = generator.pow(
+        &p_to_the_e_basis[e - 1].repr,
+        p_to_the_e_basis[e - 1].bitlen,
+    );
+    assert_eq!(
+        prime_order_subgroup_generator.equals(&Fp2::ONE),
+        FAILURE_RETVAL,
+        "g has order < 5^{}",
+        e,
+    );
+
+    let mut partial_solutions = Vec::with_capacity(e);
+    let mut partial_sum = vec![0];
+    for i in 0..e {
+        let partial_sum_bn = word_bn_to_byte_bn(&partial_sum);
+        let r = *value
+            * generator
+                .pow(&partial_sum_bn.repr, partial_sum_bn.bitlen)
+                .invert(); // TODO: can't we use the (much faster) conjugate since we're in a cyclotomic group?
+        let u = r.pow(
+            &p_to_the_e_basis[e - i - 1].repr,
+            p_to_the_e_basis[e - i - 1].bitlen,
+        );
+
+        let (x, ok) = solve_dlp_order_five(&prime_order_subgroup_generator, &u);
+        partial_solutions.push(x);
+        partial_sum = add_bn_vartime(
+            &partial_sum,
+            &mul_bn_by_u64_vartime(&byte_bn_to_word_bn(&p_to_the_e_basis[i]), x as u64),
+        );
+        retval &= ok;
+    }
+
+    (word_bn_to_byte_bn(&partial_sum), retval)
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::rstest;

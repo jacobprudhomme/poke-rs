4196ef7d632019e7027b10993724d3199f236664
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2025-12-10 20:50:18 +0100

    Return to using proper random scalars

--- src/rand.rs
+++ src/rand.rs
@@ -1,7 +1,7 @@
 use core::array;
 
 use fp2::traits::Fp2 as Fp2Trait;
-use isogeny::elliptic::{basis::BasisX, curve::Curve, point::PointX, projective_point::Point};
+use isogeny::elliptic::{curve::Curve, projective_point::Point};
 use num_bigint::{BigUint, RandBigInt as _};
 
 use crate::{FAILURE_RETVAL, SUCCESS_RETVAL, bn::BigNum};
@@ -10,10 +10,10 @@ pub fn sample_random_element_mod(modulus: &BigNum) -> BigNum {
     let mut rng = old_rand::thread_rng();
 
     let modulus = BigUint::from_bytes_le(modulus.as_le_bytes());
-    let _element = rng.gen_biguint_below(&modulus);
+    let element = rng.gen_biguint_below(&modulus);
 
-    // BigNum::new(&element.to_u64_digits())
-    BigNum::new(&[1])
+    BigNum::new(&element.to_u64_digits())
+    // BigNum::new(&[1])
 }
 
 pub fn sample_random_unit_mod(modulus: &BigNum) -> (BigNum, BigNum) {
@@ -27,15 +27,15 @@ pub fn sample_random_unit_mod(modulus: &BigNum) -> (BigNum, BigNum) {
         element = rng.gen_biguint_below(&modulus);
         inverse = element.modinv(&modulus);
     }
-    let Some(_inverse) = inverse else {
+    let Some(inverse) = inverse else {
         unreachable!("At this point, we are ensured to have an invertible element");
     };
 
-    // let element = BigNum::new(&element.to_u64_digits());
-    let element = BigNum::new(&[1]);
+    let element = BigNum::new(&element.to_u64_digits());
+    // let element = BigNum::new(&[1]);
 
-    // let inverse = BigNum::new(&inverse.to_u64_digits());
-    let inverse = BigNum::new(&[1]);
+    let inverse = BigNum::new(&inverse.to_u64_digits());
+    // let inverse = BigNum::new(&[1]);
 
     (element, inverse)
 }
@@ -75,11 +75,11 @@ pub fn sample_random_invertible_matrix_mod(
     }
     matrix[1][1] = element;
 
-    // matrix.map(|row| row.map(|element| BigNum::new(&element.to_u64_digits())))
-    [
-        [BigNum::new(&[1]), BigNum::zero()],
-        [BigNum::zero(), BigNum::new(&[1])],
-    ]
+    matrix.map(|row| row.map(|element| BigNum::new(&element.to_u64_digits())))
+    // [
+    //     [BigNum::new(&[1]), BigNum::zero()],
+    //     [BigNum::zero(), BigNum::new(&[1])],
+    // ]
 }
 
 // Randomly find a basis of the given torsion subgroup on the given curve
@@ -128,7 +128,7 @@ pub fn sample_random_torsion_basis<Fp2: Fp2Trait>(
 
     // Generate another point of the desired order, linearly independent to the first
     // Includes Weil pairing between both points
-    let (_V, _eUV) = loop {
+    let (V, eUV) = loop {
         let V = curve.rand_point(&mut rng);
 
         /* Check point is in the torsion subgroup */
@@ -188,58 +188,58 @@ pub fn sample_random_torsion_basis<Fp2: Fp2Trait>(
         break (V, eUV);
     };
 
-    let xU = PointX::from_x_coord(&Fp2::decode_reduce(&[
-        3, 36, 23, 208, 115, 217, 172, 163, 98, 15, 91, 203, 150, 201, 77, 119, 81, 212, 9, 247,
-        201, 65, 175, 184, 119, 101, 95, 190, 176, 162, 118, 222, 157, 140, 41, 11, 158, 65, 175,
-        148, 126, 87, 155, 150, 173, 161, 130, 10, 199, 233, 219, 164, 247, 6, 60, 191, 212, 194,
-        14, 110, 228, 248, 180, 81, 144, 208, 42, 227, 178, 92, 233, 102, 249, 149, 44, 248, 95,
-        45, 181, 105, 58, 109, 45, 63, 187, 75, 102, 141, 143, 114, 62, 234, 107, 47, 204, 100,
-        225, 156, 156, 112, 49, 219, 59, 73, 234, 241, 122, 2,
-    ]));
-    let xV = PointX::from_x_coord(&Fp2::decode_reduce(&[
-        29, 148, 125, 128, 17, 251, 215, 243, 121, 68, 253, 137, 133, 48, 132, 193, 207, 95, 26,
-        36, 39, 41, 163, 107, 95, 141, 213, 78, 17, 194, 51, 173, 239, 160, 184, 74, 81, 137, 37,
-        148, 250, 220, 55, 41, 185, 4, 28, 108, 246, 213, 71, 72, 173, 55, 244, 213, 77, 72, 119,
-        226, 154, 113, 30, 184, 78, 170, 0, 205, 112, 6, 254, 122, 217, 26, 224, 18, 135, 247, 104,
-        59, 240, 58, 59, 233, 200, 84, 230, 241, 188, 95, 182, 145, 43, 206, 113, 116, 219, 162,
-        177, 16, 238, 187, 54, 11, 143, 63, 170, 15,
-    ]));
-    let xUV = PointX::from_x_coord(&Fp2::decode_reduce(&[
-        32, 44, 242, 20, 116, 222, 35, 180, 40, 3, 254, 145, 117, 21, 79, 35, 71, 51, 95, 179, 235,
-        224, 244, 16, 106, 17, 43, 210, 41, 156, 219, 189, 91, 2, 155, 248, 10, 71, 75, 152, 43, 5,
-        3, 83, 69, 232, 160, 140, 194, 199, 16, 199, 93, 103, 89, 35, 213, 13, 4, 62, 89, 157, 113,
-        58, 44, 170, 0, 247, 163, 235, 153, 140, 182, 207, 136, 108, 160, 246, 139, 133, 99, 215,
-        20, 210, 66, 23, 0, 66, 171, 119, 132, 162, 254, 163, 180, 213, 139, 177, 233, 249, 76,
-        216, 73, 1, 64, 110, 254, 76,
-    ]));
-    let (U, V) = curve.lift_basis(&BasisX::from_points(&xU, &xV, &xUV));
+    // let xU = PointX::from_x_coord(&Fp2::decode_reduce(&[
+    //     3, 36, 23, 208, 115, 217, 172, 163, 98, 15, 91, 203, 150, 201, 77, 119, 81, 212, 9, 247,
+    //     201, 65, 175, 184, 119, 101, 95, 190, 176, 162, 118, 222, 157, 140, 41, 11, 158, 65, 175,
+    //     148, 126, 87, 155, 150, 173, 161, 130, 10, 199, 233, 219, 164, 247, 6, 60, 191, 212, 194,
+    //     14, 110, 228, 248, 180, 81, 144, 208, 42, 227, 178, 92, 233, 102, 249, 149, 44, 248, 95,
+    //     45, 181, 105, 58, 109, 45, 63, 187, 75, 102, 141, 143, 114, 62, 234, 107, 47, 204, 100,
+    //     225, 156, 156, 112, 49, 219, 59, 73, 234, 241, 122, 2,
+    // ]));
+    // let xV = PointX::from_x_coord(&Fp2::decode_reduce(&[
+    //     29, 148, 125, 128, 17, 251, 215, 243, 121, 68, 253, 137, 133, 48, 132, 193, 207, 95, 26,
+    //     36, 39, 41, 163, 107, 95, 141, 213, 78, 17, 194, 51, 173, 239, 160, 184, 74, 81, 137, 37,
+    //     148, 250, 220, 55, 41, 185, 4, 28, 108, 246, 213, 71, 72, 173, 55, 244, 213, 77, 72, 119,
+    //     226, 154, 113, 30, 184, 78, 170, 0, 205, 112, 6, 254, 122, 217, 26, 224, 18, 135, 247, 104,
+    //     59, 240, 58, 59, 233, 200, 84, 230, 241, 188, 95, 182, 145, 43, 206, 113, 116, 219, 162,
+    //     177, 16, 238, 187, 54, 11, 143, 63, 170, 15,
+    // ]));
+    // let xUV = PointX::from_x_coord(&Fp2::decode_reduce(&[
+    //     32, 44, 242, 20, 116, 222, 35, 180, 40, 3, 254, 145, 117, 21, 79, 35, 71, 51, 95, 179, 235,
+    //     224, 244, 16, 106, 17, 43, 210, 41, 156, 219, 189, 91, 2, 155, 248, 10, 71, 75, 152, 43, 5,
+    //     3, 83, 69, 232, 160, 140, 194, 199, 16, 199, 93, 103, 89, 35, 213, 13, 4, 62, 89, 157, 113,
+    //     58, 44, 170, 0, 247, 163, 235, 153, 140, 182, 207, 136, 108, 160, 246, 139, 133, 99, 215,
+    //     20, 210, 66, 23, 0, 66, 171, 119, 132, 162, 254, 163, 180, 213, 139, 177, 233, 249, 76,
+    //     216, 73, 1, 64, 110, 254, 76,
+    // ]));
+    // let (U, V) = curve.lift_basis(&BasisX::from_points(&xU, &xV, &xUV));
 
-    // FIXME: why the heck does the Weil pairing produce the square of what Sage produces??
-    let eUV = curve.weil_pairing(
-        &xU.x(),
-        &xV.x(),
-        &xUV.x(),
-        torsion_subgroup_order.as_le_bytes(),
-        torsion_subgroup_order.nbits(),
-    );
-    assert_eq!(
-        eUV.pow(
-            reduced_torsion_subgroup_order.as_le_bytes(),
-            reduced_torsion_subgroup_order.nbits(),
-        )
-        .equals(&Fp2::ONE),
-        FAILURE_RETVAL,
-        "",
-    );
-    assert_eq!(
-        eUV.pow(
-            torsion_subgroup_order.as_le_bytes(),
-            torsion_subgroup_order.nbits(),
-        )
-        .equals(&Fp2::ONE),
-        SUCCESS_RETVAL,
-        "",
-    );
+    // // FIXME: why the heck does the Weil pairing produce the square of what Sage produces??
+    // let eUV = curve.weil_pairing(
+    //     &xU.x(),
+    //     &xV.x(),
+    //     &xUV.x(),
+    //     torsion_subgroup_order.as_le_bytes(),
+    //     torsion_subgroup_order.nbits(),
+    // );
+    // assert_eq!(
+    //     eUV.pow(
+    //         reduced_torsion_subgroup_order.as_le_bytes(),
+    //         reduced_torsion_subgroup_order.nbits(),
+    //     )
+    //     .equals(&Fp2::ONE),
+    //     FAILURE_RETVAL,
+    //     "",
+    // );
+    // assert_eq!(
+    //     eUV.pow(
+    //         torsion_subgroup_order.as_le_bytes(),
+    //         torsion_subgroup_order.nbits(),
+    //     )
+    //     .equals(&Fp2::ONE),
+    //     SUCCESS_RETVAL,
+    //     "",
+    // );
 
     (U, V, eUV)
 }

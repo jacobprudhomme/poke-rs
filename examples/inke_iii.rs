7eaab269428d1710488763c9d5b5cd2a4ccb2931
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2026-05-22 18:38:36 +0200

    Update examples to use truly random messages and have cleaner output

--- examples/inke_iii.rs
+++ examples/inke_iii.rs
@@ -3,6 +3,7 @@ use poke::{
     inke::{decrypt, encrypt, keygen},
     params,
 };
+use rand::RngCore as _;
 
 fn main() {
     let params = params::inke_iii::get_params();
@@ -10,15 +11,22 @@ fn main() {
     let (pub_key, prv_key, ok) = keygen(&params);
     assert_eq!(ok, SUCCESS_RETVAL, "Key generation finished with errors");
 
-    let message = String::from("Hello, world!");
-    let message = message.as_bytes();
-    println!("Original message: {:?}", message);
+    let mut rng = rand::rng();
+    let mut message = [0; 32];
+    rng.fill_bytes(&mut message);
+    println!("Original message:\n{:?}\n", message);
 
-    let (ct, ok) = encrypt(&params, &pub_key, message);
+    let (ct, ok) = encrypt(&params, &pub_key, &message);
     assert_eq!(ok, SUCCESS_RETVAL, "Encryption finished with errors");
-    println!("Encrypted message: {:?}", ct.encrypted_message);
+    println!("Encrypted message:\n{:?}\n", ct.encrypted_message);
 
-    let (message, ok) = decrypt(&params, &prv_key, &ct);
+    let (dec_message, ok) = decrypt(&params, &prv_key, &ct);
     assert_eq!(ok, SUCCESS_RETVAL, "Decryption finished with errors");
-    println!("Decrypted message: {:?}", message);
+    println!("Decrypted message:\n{:?}\n", dec_message);
+
+    if dec_message == message {
+        println!("SUCCESS: Messages match!");
+    } else {
+        println!("FAILURE: Messages do not match.");
+    }
 }
--- examples/poke_i.rs
+++ examples/poke_i.rs
@@ -2,6 +2,7 @@ use poke::{
     SUCCESS_RETVAL, params,
     poke::{decrypt, encrypt, keygen},
 };
+use rand::RngCore as _;
 
 fn main() {
     let params = params::poke_i::get_params();
@@ -9,15 +10,22 @@ fn main() {
     let (pub_key, prv_key, ok) = keygen(&params);
     assert_eq!(ok, SUCCESS_RETVAL, "Key generation finished with errors");
 
-    let message = String::from("Hello, world!");
-    let message = message.as_bytes();
-    println!("Original message: {:?}", message);
+    let mut rng = rand::rng();
+    let mut message = [0; 32];
+    rng.fill_bytes(&mut message);
+    println!("Original message:\n{:?}\n", message);
 
-    let (ct, ok) = encrypt(&params, &pub_key, message);
+    let (ct, ok) = encrypt(&params, &pub_key, &message);
     assert_eq!(ok, SUCCESS_RETVAL, "Encryption finished with errors");
-    println!("Encrypted message: {:?}", ct.encrypted_message);
+    println!("Encrypted message:\n{:?}\n", ct.encrypted_message);
 
-    let (message, ok) = decrypt(&params, &prv_key, &ct);
+    let (dec_message, ok) = decrypt(&params, &prv_key, &ct);
     assert_eq!(ok, SUCCESS_RETVAL, "Decryption finished with errors");
-    println!("Decrypted message: {:?}", message);
+    println!("Decrypted message:\n{:?}\n", dec_message);
+
+    if dec_message == message {
+        println!("SUCCESS: Messages match!");
+    } else {
+        println!("FAILURE: Messages do not match.");
+    }
 }
